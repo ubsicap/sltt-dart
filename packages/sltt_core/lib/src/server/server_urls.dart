653849b2894685013af1739a4edbfccd19779b03
Eric Pyle <epyle@biblesocieties.org>
2025-07-31 18:08:10 -0500

    new kCloudDevUrl

--- packages/sltt_core/lib/src/server/server_urls.dart
+++ packages/sltt_core/lib/src/server/server_urls.dart
@@ -2,7 +2,7 @@ import 'server_ports.dart';
 
 // Cloud Dev URL for production AWS Lambda endpoint
 const String kCloudDevUrl =
-    'https://um5f10yoh1.execute-api.us-east-1.amazonaws.com/dev/';
+    'https://exz43jep09.execute-api.us-east-1.amazonaws.com/dev';
 
 // Localhost URLs for local development servers
 const String kLocalhostDownsyncsUrl = 'http://localhost:$kDownsyncsPort';
