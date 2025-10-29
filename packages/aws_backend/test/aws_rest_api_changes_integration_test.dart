b43aa98f873ec039ef791a0b610225ccc00d0563
Eric Pyle <epyle@biblesocieties.org>
2025-10-29 15:14:33 -0500

    fix tests and table debugging

--- packages/aws_backend/test/aws_rest_api_changes_integration_test.dart
+++ packages/aws_backend/test/aws_rest_api_changes_integration_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:aws_backend/src/models/dynamo_change_log_entry.dart';
 import 'package:http/http.dart' as http;
 import 'package:sltt_core/sltt_core.dart';
 import 'package:test/test.dart';
@@ -9,6 +10,11 @@ void main() {
   final baseUrl = Uri.parse(
     Platform.environment['CLOUD_BASE_URL'] ?? kCloudDevUrl,
   );
+
+  setUpAll(() {
+    // register DynamoChangeLogEntry factory for api models usage
+    dynamoChangeLogEntryFactoryRegistration;
+  });
 
   group('Integration POST /api/changes', () {
     test('POST invalid JSON returns 400', () async {
@@ -29,22 +35,43 @@ void main() {
       'POST minimal valid payload returns 200 or 201',
       () async {
         final uri = baseUrl.replace(path: '/api/changes');
-        final payload = jsonEncode({
-          'changes': [
-            {'domainId': '__test1', 'entityType': 'document', 'entityId': 'i1'},
-          ],
-          'srcStorageType': 'local',
-          'srcStorageId': 'test',
-          'storageMode': 'save',
-        });
+        final projectData = BaseDataFields(
+          parentId: 'parentId',
+          parentProp: 'parentProp',
+        );
+        final projectId = '__test_project_post_minimal_valid_payload__';
+
+        final change =
+            ChangeLogEntryFactoryService.forChangeSave<
+              DynamoChangeLogEntry,
+              int,
+              BaseDataFields
+            >(
+              factory: DynamoChangeLogEntry.new,
+              domainType: 'project',
+              domainId: projectId,
+              entityType: 'project',
+              entityId: projectId,
+              changeBy: 'userId',
+              data: projectData,
+            );
+
+        final request = CreateChangesRequest(
+          changes: [change],
+          srcStorageType: 'local',
+          srcStorageId: 'test',
+          storageMode: 'save',
+        );
+        final payload = jsonEncode(request.toJson());
         final resp = await http.post(
           uri,
           body: payload,
           headers: {'Content-Type': 'application/json'},
         );
-        expect(resp.statusCode, anyOf([200, 201]));
+        expect(resp.statusCode, anyOf([200, 201]), reason: 'Got ${resp.body}');
       },
       tags: ['internet', 'integration'],
+      timeout: Timeout.none,
     );
   });
 }
