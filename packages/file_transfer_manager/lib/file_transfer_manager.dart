84e022b343bdd70db441011e8d7e40d35c42975e
Eric Pyle <epyle@biblesocieties.org>
2025-12-26 19:07:54 -0600

    begin file_transfer_manager.dart

---
+++ packages/file_transfer_manager/lib/file_transfer_manager.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/file_transfer_manager_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
