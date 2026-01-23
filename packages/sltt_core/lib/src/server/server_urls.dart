97e09368aaaf6eeb53860c3d1ad111955cc19858
Eric Pyle <epyle@biblesocieties.org>
2026-01-23 17:16:20 -0600

    sltt-backend url

--- packages/sltt_core/lib/src/server/server_urls.dart
+++ packages/sltt_core/lib/src/server/server_urls.dart
@@ -4,4 +4,4 @@
 // (populated by Serverless Framework). If you re-deploy, re-run the
 // generator script or set SLTT_REST_API_ID to regenerate this file.
 const String kCloudDevUrl =
-    'https://w31zvo6rl5.execute-api.us-east-1.amazonaws.com/dev';
+    'https://fs9y5ey71j.execute-api.us-east-1.amazonaws.com/dev';
