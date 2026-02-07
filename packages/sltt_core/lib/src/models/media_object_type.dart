a3d2b3c44be3278f227f52c0b2f12fe590a42583
Eric Pyle <epyle@biblesocieties.org>
2026-02-07 12:29:31 -0600

    generateId

--- packages/sltt_core/lib/src/models/media_object_type.dart
+++ packages/sltt_core/lib/src/models/media_object_type.dart
@@ -27,4 +27,22 @@ enum MediaObjectType {
     kMediaObjectTypeVideoChat: 'viCh',
     kMediaObjectTypeBlob: 'blob',
   };
+
+  /// Generate a unique media object ID with embedded entity type short suffix
+  /// Format: YYYY-mmdd-HHMMss-sss±HH{UC}-{4-character-random}-{entity-short}
+  /// Similar to generateCid() but without -cid suffix
+  /// ({String? [userId]}) embed 2 character hash of the userId after the timezone hour offset, 'UK' by default
+  static String generateId({
+    required MediaObjectType mediaObjectType,
+    String? userId,
+  }) {
+    final suffix = getSuffix(mediaObjectType: mediaObjectType);
+    final entityId = generateCoreId(userId: userId, suffix: suffix);
+    return entityId;
+  }
+
+  static String getSuffix({required MediaObjectType mediaObjectType}) {
+    return suffixMapping[mediaObjectType.value] ??
+        mediaObjectType.value.substring(0, 4).padRight(4, 'Z');
+  }
 }
