f00c08f48a33ec359d25ce38e51a585c217a63c9
Eric Pyle <epyle@biblesocieties.org>
2026-07-15 16:20:12 -0500

    update kCloudDevUrl

--- packages/sltt_core/lib/src/server/server_urls.dart
+++ packages/sltt_core/lib/src/server/server_urls.dart
@@ -4,4 +4,4 @@
 // (populated by Serverless Framework). If you re-deploy, re-run the
 // generator script or set SLTT_REST_API_ID to regenerate this file.
 const String kCloudDevUrl =
-    'https://o0rkznjapb.execute-api.us-east-1.amazonaws.com/dev';
+    'https://uu29hqgay7.execute-api.us-east-1.amazonaws.com/dev';
